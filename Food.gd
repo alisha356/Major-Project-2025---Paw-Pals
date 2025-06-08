fb062e10c2d3374da891a88c164e752b7e89ea9b
Alisha <21amessingham@wakatipu.school.nz>
2025-06-08 22:10:48 +1200

    Food, hunger, and coins
    Given the user 5x food to start and made it so if the fish is pressed the cat is fed. If the cat is full then the user can't feed the cat any more. Also displayed the user's coins to start, though I don't think my method is going to work so it will have to be changed.

---
+++ Food.gd
@@ -0,0 +1,64 @@
+extends Sprite2D
+
+func _display_num_food():
+	$"../Zero".visible = false
+	$"../One".visible = false
+	$"../Two".visible = false
+	$"../Three".visible = false
+	$"../Four".visible = false
+	$"../Five".visible = false
+	$"../Six".visible = false
+	$"../Seven".visible = false
+	$"../Eight".visible = false
+	$"../Nine".visible = false
+	$"../Zero_10".visible = false
+	if Global.player_food == 0:
+		$"../Zero".visible = true
+	elif Global.player_food == 1:
+		$"../One".visible = true
+	elif Global.player_food == 2:
+		$"../Two".visible = true
+	elif Global.player_food == 3:
+		$"../Three".visible = true
+	elif Global.player_food == 4:
+		$"../Four".visible = true
+	elif Global.player_food == 5:
+		$"../Five".visible = true
+	elif Global.player_food == 6:
+		$"../Six".visible = true
+	elif Global.player_food == 7:
+		$"../Seven".visible = true
+	elif Global.player_food == 8:
+		$"../Eight".visible = true
+	elif Global.player_food == 9:
+		$"../Nine".visible = true
+	elif Global.player_food == 10:
+		$"../One".visible = true
+		$"../Zero_10".visible = true
+		
+func _display_hunger_bar():
+	$"../Hunger 8".visible = false
+	$"../Hunger 9".visible = false
+	$"../Hunger 10".visible = false
+	if Global.grey_cat_hunger == 8:
+		$"../Hunger 8".visible = true
+	elif Global.grey_cat_hunger == 9:
+		$"../Hunger 9".visible = true
+	elif Global.grey_cat_hunger == 10:
+		$"../Hunger 10".visible = true
+
+func _ready():
+	_display_num_food()
+	_display_hunger_bar()
+
+func _on_button_pressed():
+	if Global.player_food == 0:
+		pass
+	elif Global.grey_cat_hunger == 10:
+		pass
+	else:
+		Global.player_food -= 1
+		Global.grey_cat_hunger += 1
+		_display_num_food()
+		_display_hunger_bar()
+
---
+++ Global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var player_food = 5
+var player_coins = 50
+var grey_cat_hunger = 8
+
+
+#var character = "Orange Player"
+#var score = 0
+#var deaths = 0
+#var seconds = 0
+#var minutes = 0
+#var time = "0" + str(minutes)+ " : 0" + str(seconds)
+#@onready var score = $"../Player/Camera2D/Score"
+#
+#
+#func _on_body_entered(body):
+	#if body.name == "Player":
+		#queue_free()
+		#global.score += 1
+		#score.text = "Score; " + str(global.score)
