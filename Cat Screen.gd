5b841f1ba7d8736ec8468272829b17c40aaf270c
Alisha <21amessingham@wakatipu.school.nz>
2025-06-18 22:02:58 +1200

    Depleting Hunger
    Makes the cat hungrier every 30 seconds and updates hunger bar to reflect that.

---
+++ Cat Screen.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+func _display_hunger_bar():
+	$"Hunger 0".visible = false
+	$"Hunger 1".visible = false
+	$"Hunger 2".visible = false
+	$"Hunger 3".visible = false
+	$"Hunger 4".visible = false
+	$"Hunger 5".visible = false
+	$"Hunger 6".visible = false
+	$"Hunger 7".visible = false
+	$"Hunger 8".visible = false
+	$"Hunger 9".visible = false
+	$"Hunger 10".visible = false
+	if Global.grey_cat_hunger == 0:
+		$"Hunger 0".visible = true
+	elif Global.grey_cat_hunger == 1:
+		$"Hunger 1".visible = true
+	elif Global.grey_cat_hunger == 2:
+		$"Hunger 2".visible = true
+	elif Global.grey_cat_hunger == 3:
+		$"Hunger 3".visible = true
+	elif Global.grey_cat_hunger == 4:
+		$"Hunger 4".visible = true
+	elif Global.grey_cat_hunger == 5:
+		$"Hunger 5".visible = true
+	elif Global.grey_cat_hunger == 6:
+		$"Hunger 6".visible = true
+	elif Global.grey_cat_hunger == 7:
+		$"Hunger 7".visible = true
+	elif Global.grey_cat_hunger == 8:
+		$"Hunger 8".visible = true
+	elif Global.grey_cat_hunger == 9:
+		$"Hunger 9".visible = true
+	elif Global.grey_cat_hunger == 10:
+		$"Hunger 10".visible = true
+
+func _ready():
+	$Fish/Number.text = str(Global.player_food)
+
+func _process(_delta: float):
+	_display_hunger_bar()
+
+func _on_button_pressed():
+	if Global.player_food == 0:
+		pass
+	elif Global.grey_cat_hunger == 10:
+		pass
+	else:
+		Global.player_food -=1
+		Global.grey_cat_hunger += 1
+		$Fish/Number.text = str(Global.player_food)
+		_display_hunger_bar()
+		if Global.grey_cat_hunger == 10:
+			Global.player_coins += 2
+
--- Global.gd
+++ Global.gd
@@ -3,3 +3,11 @@ extends Node
 var player_food = 5
 var player_coins = 20
 var grey_cat_hunger = 8
+var cooldown = 30
+
+func _process(delta: float):
+	cooldown -= delta
+	if round(cooldown) == 0:
+		grey_cat_hunger -= 1
+		cooldown = 30
+
